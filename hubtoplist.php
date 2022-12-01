6306532aac6b4f74a5b46976f18eb61c3bcb26f4
Davor <novoselacdavor@gmail.com>
2022-12-01 14:26:39 +0100

    initial commit file

---
+++ hubtoplist.php
@@ -0,0 +1,10 @@
+<?php
+
+/**
+ * Plugin name: Hub Top List
+ * Version: 1.0
+ * Author: Davor Novoselac
+ * Description: This is a test plugin for Carl Farrugia Galea
+ * License: GPL v2 or later
+ * License URI: https://www.gnu.org/licenses/gpl-2.0.html
+*/
